412e4675677898d32a1c3ca24576d0d9fb563a11
Brendon <hall.brendonp@gmail.com>
2023-12-21 00:00:28 -0600

    move functions to other folder

---
+++ handlers/handlers.py
@@ -0,0 +1,81 @@
+import requests as requests1
+import json
+
+def openLibrary_extract_title(openLibrary, output):
+    output['title'] = openLibrary['title']
+def openLibrary_extract_authors(openLibrary, output):
+    authors = ""
+    count = 0
+    for author in enumerate(openLibrary["authors"]):
+        for auth in author:
+            if type(auth) is dict:
+                author_raw = requests1.get(f'https://openlibrary.org'+auth['key']+".json")
+                author_obj = json.loads(author_raw.text)
+                if count == 0:
+                    authors = author_obj["name"]
+                    count += 1
+                    continue
+                authors = authors + ", " + author_obj["name"]
+                count += 1
+    output['authors'] = authors
+def openLibrary_extract_subjects(openLibrary, output):
+    subjects = ", ".join(openLibrary['subjects'])
+    # subjects = ""
+    # for index, subject in enumerate(openlibrary_dict["subjects"]):
+    #     if index == 0:
+    #         subjects = subject
+    #         continue
+    #     subjects = subjects + ", " + subject
+    output['subjects'] = subjects
+def openLibrary_extract_pages(openLibrary, output):
+    output['pages'] = openLibrary["number_of_pages"]
+def openLibrary_extract_description(openLibrary, output):
+    output['description'] = openLibrary['description']['value']
+def openLibrary_process_elements(openLibrary, output):
+    if 'title' in openLibrary and output['title'] == "PENDING":
+        openLibrary_extract_title(openLibrary, output)
+    if "authors" in openLibrary and output['authors'] == "PENDING":
+        openLibrary_extract_authors(openLibrary, output)
+    if "subjects" in openLibrary and output['subjects'] == "PENDING":
+        openLibrary_extract_subjects(openLibrary, output)
+    if 'number_of_pages' in openLibrary and output['pages'] == "PENDING":
+        openLibrary_extract_pages(openLibrary, output)
+    if 'description' in openLibrary and output['description'] == "PENDING":
+        openLibrary_extract_description(openLibrary, output)
+
+
+
+def google_extract_authors(google, output):
+    authors = ", ".join(google['authors'])
+    # authors = ''
+    # for index, author in enumerate(google_dict['authors']):
+    #     if index == 0:
+    #         authors = author
+    #         continue
+    #     authors = authors + ', ' + author
+    output['authors'] = authors
+def google_extract_title(google, output):
+    output['title'] = google['title']
+def google_extract_pages(google, output):
+    output["pages"] = google['pageCount']
+def google_extract_description(google, output):
+    output['description'] = google['description']
+def google_extract_language(google, output):
+    output['language'] = google['language']
+def google_extract_thumbnail(google, output):
+    if 'thumbnail' in google['imageLinks']:
+        thumbnail = google['imageLinks']['thumbnail']
+        output['thumbnail'] = thumbnail
+def google_process_elements(google, output):
+    if 'authors' in google and output['authors'] == 'PENDING':
+        google_extract_authors(google, output)
+    if 'title' in google and output['title'] == 'PENDING':
+        google_extract_title(google,output)
+    if 'pageCount' in google and output['pages'] == "PENDING":
+        google_extract_pages(google, output)
+    if 'description' in google and output['description'] == 'PENDING':
+        google_extract_description(google, output)
+    if 'language' in google and output['language'] == "PENDING":
+        google_extract_language(google, output)
+    if 'imageLinks' in google and len(google["imageLinks"])>0 and output['thumbnail'] == "PENDING":
+        google_extract_thumbnail(google, output)
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import requests as requests1
 import json
 from openpyxl.workbook import Workbook
 
+from handlers.handlers import google_process_elements, openLibrary_process_elements
+
+
 import os.path
 
 from google.auth.transport.requests import Request
@@ -65,79 +68,16 @@ while isCapture == True:
                 'title': 'PENDING',
                 'isbn': int(code_number)
             }
-            openlibrary_raw = requests1.get(f"https://openlibrary.org/isbn/{code_number}.json")
-            if openlibrary_raw.status_code == 200:    
-                openlibrary_dict = json.loads(openlibrary_raw.text)
 
-                if 'title' in openlibrary_dict:
-                    output['title'] = openlibrary_dict['title']
+            with requests1.get(f"https://openlibrary.org/isbn/{code_number}.json") as openlibrary_raw:
+                if openlibrary_raw.status_code == 200:
+                    openLibrary_process_elements(json.loads(openlibrary_raw.text), output)
 
-                if "authors" in openlibrary_dict:
-                    authors = ""
-                    count = 0
-                    for author in enumerate(openlibrary_dict["authors"]):
-                        for auth in author:
-                            if type(auth) is dict:
-                                author_raw = requests1.get(f'https://openlibrary.org'+auth['key']+".json")
-                                author_obj = json.loads(author_raw.text)
-                                if count == 0:
-                                    authors = author_obj["name"]
-                                    continue
-                                authors = authors + ", " + author_obj["name"]
-                    
-                    output['authors'] = authors
+            with requests1.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + code_number) as google_raw:
+                if google_raw.status_code == 200:    
+                    google_process_elements(json.loads(google_raw.text)['items'][0]['volumeInfo'], output)
 
-                
-                if "subjects" in openlibrary_dict:
-                    subjects = ""
-                    for index, subject in enumerate(openlibrary_dict["subjects"]):
-                        if index == 0:
-                            subjects = subject
-                            continue
-                        subjects = subjects + ", " + subject
-                    output['subjects'] = subjects
 
-                if 'number_of_pages' in openlibrary_dict:
-                    pages = openlibrary_dict["number_of_pages"]
-                    output['pages'] = pages
-
-                if 'description' in openlibrary_dict:
-                    description = openlibrary_dict['description']['value']
-                    output['description'] = description
-
-            google_raw = requests1.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + code_number)
-            
-            if google_raw.status_code == 200:    
-                google_dict = json.loads(google_raw.text)['items'][0]['volumeInfo']
-
-                if 'authors' in google_dict and output['authors'] == 'PENDING':
-                    authors = ''
-                    for index, author in enumerate(google_dict['authors']):
-                        if index == 0:
-                            authors = author
-                            continue
-                        authors = authors + ', ' + author
-                    output['authors'] = authors
-
-                if 'title' in google_dict and output['title'] == 'PENDING':
-                    output['title'] = google_dict['title']
-
-                if 'pageCount' in google_dict and output['pages'] == "PENDING":
-                    output["pages"] = google_dict['pageCount']
-
-                if 'description' in google_dict and output['description'] == 'PENDING':
-                    output['description'] = google_dict['description']
-                
-                if 'language' in google_dict:
-                    language = google_dict['language']
-                    output['language'] = language
-
-                if 'imageLinks' in google_dict and len(google_dict["imageLinks"])>0:
-                    if 'thumbnail' in google_dict['imageLinks']:
-                        thumbnail = google_dict['imageLinks']['thumbnail']
-                        output['thumbnail'] = thumbnail
-
-            headers = ['Title', 'Authors', 'Subjects', 'Language', 'Subjects', 'ISBN', 'Description', 'thumbnail']
             
             # If modifying these scopes, delete the file token.json.
             SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
